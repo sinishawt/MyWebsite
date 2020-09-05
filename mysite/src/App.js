042918f6e320a472f235e5158d1e2fa074645a82
sinishawt <59536151+sinishawt@users.noreply.github.com>
2020-09-04 22:05:03 -0500

    logo

--- mysite/src/App.js
+++ mysite/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://media.giphy.com/media/3o6nVbF4uoHsV6Vqne/giphy.gif" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h2>My Site Working on it!</h2>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
